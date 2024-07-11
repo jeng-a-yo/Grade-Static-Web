d42ac7fd976eb45dc6127a83d5f6f344561a0278
jackyyoyo2@gmail.com <jackyyoyo2@gmail.com>
2024-07-11 13:01:48 +0800

    add detail_classify

---
+++ detail_classify.py
@@ -0,0 +1,7 @@
+from timer import timing_decorator
+import os
+import shutil
+
+source_dirs = ["國文", "英文", "數學", "自然", "社會"]
+chinese_dir = ["國文", "國寫"]
+test_judge_token = []
--- timer.py
+++ timer.py
@@ -1,13 +1,15 @@
 import time
 from functools import wraps
 
+
 def timing_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start_time = time.time()  # Record start time
-        result = func(*args, **kwargs)  # Execute the function
-        end_time = time.time()  # Record end time
-        elapsed_time = end_time - start_time  # Calculate elapsed time
-        print(f"Function {func.__name__!r} executed in {elapsed_time:.4f} seconds")
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("================================================================")
+        print(f"Function '{func.__name__}' executed in {elapsed_time:.6f} seconds")
         return result
     return wrapper
